8c39cb1754ef0f406d1018e5adc80fea53bea47c
Honza Široký <honza@siroky.io>
2023-12-01 12:53:59 +0100

    Day 1 done.

---
+++ Extensions.cs
@@ -0,0 +1,29 @@
+﻿namespace AOC;
+
+public static class Extensions
+{
+    public static int ToInt(this string s)
+    {
+        return Int32.Parse(s);
+    }
+
+    public static int ToInt(this char c)
+    {
+        return c.ToString().ToInt();
+    }
+
+    public static bool IsDigit(this char c)
+    {
+        return Char.IsDigit(c);
+    }
+
+    public static IEnumerable<int> To(this int start, int end)
+    {
+        var increment = start > end ? -1 : 1;
+        var boundary = end + increment;
+        for (var i = start; i != boundary; i += increment)
+        {
+            yield return i;
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -4,6 +4,57 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        var lines = File.ReadAllLines("../../../Inputs/01.txt");
+
+        Console.WriteLine(lines.Select(l => GetValue(l, textual: false)).Sum());
+        Console.WriteLine(lines.Select(l => GetValue(l, textual: true)).Sum());
     }
+
+    private static int GetValue(string line, bool textual)
+    {
+        var firstDigit = GetDigit(line, reverse: false, textual);
+        var lastDigit = GetDigit(line, reverse: true, textual);
+        return 10 * firstDigit + lastDigit;
+    }
+
+    private static int GetDigit(string line, bool reverse, bool textual)
+    {
+        var lastIndex = line.Length - 1;
+        var startIndex = reverse ? lastIndex : 0;
+        var endIndex = reverse ? 0 : lastIndex;
+
+        foreach (var index in startIndex.To(endIndex))
+        {
+            if (line[index].IsDigit())
+            {
+                return line[index].ToInt();
+            }
+            if (textual)
+            {
+                for (var d = 0; d < Digits.Count; d++)
+                {
+                    var digit = Digits[d];
+                    if (line.Substring(index).StartsWith(digit))
+                    {
+                        return d + 1;
+                    }
+                }
+            }
+        }
+
+        return 0;
+    }
+
+    private static List<string> Digits = new List<string>
+    {
+        "one",
+        "two",
+        "three",
+        "four",
+        "five",
+        "six",
+        "seven",
+        "eight",
+        "nine"
+    };
 }
